48ccddb336514b59a014c4b1ac8c84c698647fbb
lue li <luel@andrew.cmu.edu>
2019-12-03 19:31:01 -0500

    simplify data getting process to save time

--- main.py
+++ main.py
@@ -1,18 +1,14 @@
 import preprocess_data
 import numpy as np
 from helper_function import combine_data_and_weights
-pos_path = "./review_polarity/txt_sentoken/pos/cv000_29590.txt"
-neg_path = "./review_polarity/txt_sentoken/neg/cv000_29416.txt"
 
-pos_reviews = preprocess_data.preprocess_data(pos_path)
 
-pos_cnn_data_train, pos_embedding_weights_train, pos_cnn_data_test, pos_embedding_weights_test = preprocess_data.split_and_tokenize(
-    pos_reviews)
-
-pos_data_train = combine_data_and_weights(
-    pos_cnn_data_train, pos_embedding_weights_train)
-pos_data_test = combine_data_and_weights(
-    pos_cnn_data_test, pos_embedding_weights_test)
-
-print(pos_data_test)
-print(pos_data_train)
+def get_data(path):
+    reviews = preprocess_data.preprocess_data(path)
+    cnn_data_train, embedding_weights_train, cnn_data_test, embedding_weights_test = preprocess_data.split_and_tokenize(
+        reviews)
+    pos_data_train = combine_data_and_weights(
+        cnn_data_train, embedding_weights_train)
+    pos_data_test = combine_data_and_weights(
+        cnn_data_test, embedding_weights_test)
+    return pos_data_train, pos_data_test
---
+++ write_training_data_to_csv.py
@@ -0,0 +1,37 @@
+from main import get_data
+import csv
+import numpy as np
+pos_path = "./review_polarity/txt_sentoken/pos/cv000_29590.txt"
+neg_path = "./review_polarity/txt_sentoken/neg/cv000_29416.txt"
+pos_data_train, pos_data_test = get_data(pos_path)
+neg_data_train, neg_data_test = get_data(neg_path)
+print("shape of pos data train")
+print(pos_data_train.shape)
+print("shape of pos data test")
+print(pos_data_test.shape)
+print("shape of neg data train")
+print(neg_data_train.shape)
+print("shape of neg data test")
+print(neg_data_test.shape)
+with open('training_data_pos.csv', 'w',) as file:
+    writer = csv.writer(file)
+    for review in pos_data_train:
+        for word in review:
+            writer.writerow(word)
+with open('testing_data_pos.csv', 'w',) as file:
+    writer = csv.writer(file)
+    for review in pos_data_test:
+        for word in review:
+            writer.writerow(word)
+
+with open('testing_data_neg.csv', 'w',) as file:
+    writer = csv.writer(file)
+    for review in neg_data_test:
+        for word in review:
+            writer.writerow(word)
+
+with open('training_data_neg.csv', 'w') as file:
+    writer = csv.writer(file)
+    for review in neg_data_train:
+        for word in review:
+            writer.writerow(word)
